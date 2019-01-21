47250adca0b4386175a2b350420d83d99037df87
Aureliano Yepez <ayepez2020@berkeley.edu>
2019-01-20 20:10:32 -0800

    more added

--- src/algs/uniquePaths/uniquePaths.go
+++ src/algs/uniquePaths/uniquePaths.go
@@ -1,18 +1,44 @@
 // Unique Paths -> determaines the number of unique paths from (m,n) at the top left on a grid to
 // the bottom right, end
 // Note -> m rows n columns  -> m by n
+// Problem TO DO
 package main
 
-func setUpGrid(m int, n int) int {
-	return 0
+import (
+	"fmt"
+)
+
+func uniquePaths(m int, n int) int {
+	waysToMake := make([][]int, n)
+	for i := 0; i < n; i++ {
+		waysToMake[i] = make([]int, m)
+	}
+	if m == 1 || n == 1 {
+		return 1
+	} else {
+		// rows
+		for i := 0; i < n; i++ {
+			waysToMake[i][0] = 1
+		}
+		// cols
+		for j := 0; j < m; j++ {
+			waysToMake[0][j] = 1
+		}
+		// 1 accounts for the offset
+		for i := 1; i < n; i++ {
+			for j := 1; j < m; j++ {
+				waysToMake[i][j] = waysToMake[i-1][j] + waysToMake[i][j-1]
+			}
+		}
+	}
+	return waysToMake[n-1][m-1]
 }
 
-func uniquePaths() int {
-
-	return 0
-}
 func main() {
-
+	n := 3
+	m := 7
+	uniquePathNum := uniquePaths(m, n)
+	fmt.Println(uniquePathNum)
 }
 
 // 1. want to create m by n template to iterate
@@ -26,3 +52,38 @@ func main() {
 // bfs find all neighboring distance, similar to shortest path but without weights
 // dfs explores all nodes from top to its depth
 // can only go up and down i.e left and right
+// from m what are all the possible paths
+// from m + 1 what are all the posible paths
+// from n what are all the possible paths
+// from n + 1 what are all the possible paths
+// from m,n what are all the possible paths
+// from m - 1, n - 1 what are all the possible paths ?
+// how can we use our resources to generate a path
+// want to run the path n time ?
+
+// given a 2 * 2 grid
+/*
+(1,0)
+  x  x
+  x  x
+
+  Possible paths ->
+	   right  right down
+  1 -> (0,0), (1,0), (1,1)
+		down  right right
+  2 -> (0,0), (0,1), (1,1)
+  2 total paths
+  row, col
+  start position -> (0, n)
+  Given that we start at (0,n) find all possible paths to (m,n)
+  path from (m, n - 1)
+  path from (m - 1, n) down
+  start = 0 right condition
+  (0,n) -> (1,n) -> (2,n) -> (start + 1) < m
+  (0, n+1) ()
+  // keep track of indexes
+  from (m,n-1) have we reached the next node before ?
+  from (m, n-1) or (m - 1, n) is there a path to top left
+
+
+*/
---
+++ src/algs/uniquePaths/uniquePathsII.go
@@ -0,0 +1,5 @@
+package uniquePaths
+
+func main() {
+
+}
