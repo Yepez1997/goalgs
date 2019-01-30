89a5dfeea7fc91fee2a8cf4045edf878d0fb92ef
Aureliano Yepez <yepez4845@gmail.com>
2019-01-30 14:50:35 -0800

    more to add

--- src/algs/uncommonWordsTwoSentences/uncommonWords.go
+++ src/algs/uncommonWordsTwoSentences/uncommonWords.go
@@ -11,12 +11,22 @@ func uncommonWords(s string, t string) []string {
 
 	count := make(map[string]int)
 	for j := 0; j < len(sWords); j++ {
-		count[sWords[j]] = 1
+		// never makes it in here
+		if _, ok := count[sWords[j]]; ok {
+			fmt.Println("HERE")
+			count[sWords[j]]++
+
+		} else {
+			count[sWords[j]] = 1
+		}
 	}
 
 	for i := 0; i < len(tWords); i++ {
+		// never makes it in here
+		fmt.Println(tWords[i])
 		if _, ok := count[tWords[i]]; ok {
 			count[tWords[i]]++
+
 		} else {
 			count[tWords[i]] = 1
 		}
@@ -24,6 +34,7 @@ func uncommonWords(s string, t string) []string {
 
 	results := []string{}
 	for key, value := range count {
+		fmt.Println(value)
 		if value == 1 {
 			//fmt.Println(key)
 			results = append(results, key)
@@ -37,8 +48,12 @@ func uncommonWords(s string, t string) []string {
 
 func main() {
 	// testing words
-	s := "this apple is sweet"
-	t := "this apple is sour"
-	result := uncommonWords(s, t)
-	fmt.Println(result)
+	//s := "this apple is sweet"
+	//t := "this apple is sour"
+	a := "apple apple"
+	b := "banana"
+	//result := uncommonWords(s, t)
+	result2 := uncommonWords(a, b)
+	//fmt.Println(result)
+	fmt.Println(result2)
 }
