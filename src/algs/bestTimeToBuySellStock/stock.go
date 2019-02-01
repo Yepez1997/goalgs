e64493d2ad113de3fe27691b409136e3a373fec1
Aureliano Yepez <yepez4845@gmail.com>
2019-02-01 08:03:54 -0800

    stock.go

--- src/algs/bestTimeToBuySellStock/stock.go
+++ src/algs/bestTimeToBuySellStock/stock.go
@@ -4,7 +4,6 @@ import "fmt"
 
 // elts of size 2 go program needs fix
 
-// selling price must be larger
 func sellStock(prices []int) int {
 	fmt.Println("enter")
 	runningSum := 0
@@ -12,7 +11,6 @@ func sellStock(prices []int) int {
 	for i := 0; i < len(prices); i++ {
 		low := prices[i]
 		current := i
-		fmt.Println(low)
 		for low <= prices[current] && current < len(prices)-1 {
 			runningSum = (prices[current+1] - low)
 			//fmt.Println(prices[current])
@@ -27,7 +25,6 @@ func sellStock(prices []int) int {
 	return maxSum
 }
 
-// buy low sell high
 func main() {
 	arr := []int{7, 1, 5, 3, 6, 4}
 	profit := sellStock(arr)
@@ -35,7 +32,7 @@ func main() {
 	//noProfit := sellStock(arr2)
 	arr3 := []int{1, 2}
 	errors := sellStock(arr3)
-	 prfmt.Println(profit)
+	fmt.Println(profit)
 	//fmt.Println(noProfit)
 	fmt.Println(errors)
 	// [1,2]
